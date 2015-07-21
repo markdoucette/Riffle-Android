8a5a6ea04adc9bb2d727124e8a53cdcaed880954
Mark Doucette <mark@rhythmtech.ca>
2015-07-21 16:39:31 -0700

    Tidying up

--- app/src/main/java/ca/rhythmtech/riffle/activity/DisplayTripsActivity.java
+++ app/src/main/java/ca/rhythmtech/riffle/activity/DisplayTripsActivity.java
@@ -88,11 +88,6 @@ public class DisplayTripsActivity extends Activity implements AdapterView.OnItem
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 
